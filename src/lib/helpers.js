b4217e01bf95f75c4726158c91c212aa41814c84
Kari <kari_steinn@outlook.com>
2019-11-21 12:55:50 +0000

    Bætti við falli sem gefur upp random dagsetningu

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -38,3 +38,45 @@ export function el(name, ...children) {
 export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+/**
+ * Skilar random dagsetningu frá 16. júní 1995 til dagsins í dag á
+ * forminu YYYY--MM--DD.
+ */
+export function randomDate() {
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const year = randomNumber(1995, currentYear);
+  let month;
+  if (year === currentYear) {
+    month = randomNumber(1, currentMonth);
+  } else if (year === 1995) {
+    month = randomNumber(6, 12);
+  } else {
+    month = randomNumber(1, 12);
+  }
+
+  let day;
+  if (month === currentMonth && year === currentYear) {
+    day = randomNumber(1, currentDay);
+  } else if (year === 1995 && month === 6) {
+    day = randomNumber(16, 30);
+  } else if (month === 1 || month === 3 || month === 5 || month === 7
+    || month === 8 || month === 10 || month === 12) {
+    day = randomNumber(1, 31);
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    day = randomNumber(1, 30);
+  } else {
+    day = randomNumber(1, 28);
+  }
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  return `${year}-${month}-${day}`;
+}
